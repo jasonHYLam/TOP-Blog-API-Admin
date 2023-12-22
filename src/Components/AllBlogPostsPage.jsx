187c74e694766a2e854a5a5f3ab58fbbe06bd805
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-22 08:20:33 +0000

    Create Basic Fetch For AllBlogPostsPage

---
+++ src/Components/AllBlogPostsPage.jsx
@@ -0,0 +1,23 @@
+import { useLoaderData } from "react-router-dom"
+
+export async function AllBlogPostsLoader() {
+    console.log('attempting all fetch')
+    return await fetch('http://localhost:3000/home', {
+        // I may need to pass JWT for this?
+    })
+    .then(res => res.json())
+
+}
+
+export function AllBlogPostsPage() {
+
+    const { allPosts } = useLoaderData();
+    console.log(allPosts)
+
+    return (
+        <>
+        <p>All posts</p>
+        
+        </>
+    )
+}
