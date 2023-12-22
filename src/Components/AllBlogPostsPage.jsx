6df1038eefcc688beb6190b3f7d766ebfebf7d0a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-22 10:53:41 +0000

    Fix Fetch For AllPosts And Make CreateBlogPostPage

--- src/Components/AllBlogPostsPage.jsx
+++ src/Components/AllBlogPostsPage.jsx
@@ -2,7 +2,7 @@ import { useLoaderData } from "react-router-dom"
 
 export async function allBlogPostsLoader() {
     console.log('attempting all fetch')
-    return await fetch('http://localhost:3000/home', {
+    return await fetch('http://localhost:3000/admin_all_posts', {
         // I may need to pass JWT for this?
         credentials: "include"
     })
@@ -10,11 +10,14 @@ export async function allBlogPostsLoader() {
 
 }
 
+// i should probably use the User object... should i redirect? perhaps in the loader function?
+
 export function AllBlogPostsPage() {
 
     const { allPosts, user } = useLoaderData();
     console.log(allPosts)
-    console.log(`checking user: ${user}`)
+    console.log(`checking user:`)
+    console.log(user)
 
     return (
         <>
---
+++ src/Components/CreateBlogPostPage.jsx
@@ -0,0 +1,10 @@
+export function CreateBlogPostPage() {
+    return (
+        <>
+            <main>
+                <h1>Create a Blog Post</h1>
+            </main>
+        </>
+    )
+
+}
