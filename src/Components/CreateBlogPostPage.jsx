a5647a1349544b8f292d97f56e73a660bbda8a72
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-22 18:50:50 +0000

    Create Form With TinyMCE And Fetch Post Request

--- src/Components/CreateBlogPostPage.jsx
+++ src/Components/CreateBlogPostPage.jsx
@@ -1,15 +1,32 @@
 import { Editor } from '@tinymce/tinymce-react';
-console.log(import.meta.env.VITE_TINY_MCE_API_KEY )
+import { Form } from 'react-router-dom';
 
 export function CreateBlogPostPage() {
+    // hm. how to handle submit...
+
+    // create fetch?
+    async function onSubmit(data) {
+        await fetch('http://localhost:3000/login_admin')
+
+    }
+
     return (
         <>
             <main>
                 <h1>Create a Blog Post</h1>
-                <Editor
-                apiKey={import.meta.env.VITE_TINY_MCE_API_KEY}
-                
-                />
+
+                <Form method='POST' onSubmit={}>
+                    <label htmlFor="title">Title</label>
+                    <input name='title' type="text" />
+
+                    <Editor
+                    apiKey={import.meta.env.VITE_TINY_MCE_API_KEY}
+                    init={{
+                        plugins: 'link lists'
+                    }}
+                    />
+                    <button >Submit</button>
+                </Form>
             </main>
         </>
     )
