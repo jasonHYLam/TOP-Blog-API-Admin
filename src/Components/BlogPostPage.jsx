a76dc65711c46f06d504a07b5623096c8bdd35bc
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-12-24 14:56:32 +0000

    Use Html-React-Parser To Parse HTML Blog Content

--- src/Components/BlogPostPage.jsx
+++ src/Components/BlogPostPage.jsx
@@ -1,26 +1,48 @@
-import { useParams } from "react-router-dom"
+import { useLoaderData, useParams } from "react-router-dom"
+import parse from 'html-react-parser';
+
 export async function blogPostPageLoader({params}) {
     console.log('checking out params from the loader')
     console.log(params)
     console.log('before fetch')
-    const response = await fetch(`http://localhost:3000/admin_blog_post${params}`, {
+    const response = await fetch(`http://localhost:3000/admin_blog_post/${params.postid}`, {
         credentials: 'include',
     })
+    const data = await response.json()
     console.log('after fetch')
-    console.log('checking out response')
-    console.log(response)
-    return response
+    console.log('checking out data')
+    console.log(data)
+    return data
 }
-export function BlogPostPage() {
-    const postId = useParams();
 
-    console.log('checking out the params from the component')
-    console.log(postId)
+export function BlogPostPage() {
+
+    const { blogPost, comments } = useLoaderData()
+    console.log('checking out blogPost')
+    console.log(blogPost)
+    
+    console.log('checking out comments')
+    console.log(comments)
+    // i need to parse the HTML somehow
+
+    // maybe react html parser is what i need
+
+    console.log('checking out blogPost content before parsing')
+    console.log(blogPost.content)
+    console.log('checking out blogPost content after parsing')
+    console.log(parse(blogPost.content))
     
     return (
         <>
+        <button>Delete</button>
+        <button>Publish</button>
+        <button>Edit</button>
         <section>
-            <p>Let's see some</p>
+            <h1>{blogPost.title}</h1>
+
+            {parse(blogPost.content)}
+
+            
         </section>
         </>
     )
